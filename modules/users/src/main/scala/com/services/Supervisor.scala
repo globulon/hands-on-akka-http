39925be24fd8a57d75735b2d94a690f4cccb6cac
Marc-Daniel Ortega <globulon@gmail.com>
2020-07-29 14:46:17 +0200

    [FEAT] Add suprvising actor

--- modules/users/src/main/scala/com/services/Supervisor.scala
+++ modules/users/src/main/scala/com/services/Supervisor.scala
@@ -4,6 +4,8 @@ import akka.actor.typed.{Behavior, PostStop, Signal}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 final class Supervisor private[services](context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
+  context.log.info("Supervisor started")
+
   override def onMessage(msg: Nothing): Behavior[Nothing] = Behaviors.unhandled[Nothing]
 
   override def onSignal: PartialFunction[Signal, Behavior[Nothing]] = {
--- modules/users/src/main/scala/com/services/http/engine/Server.scala
+++ modules/users/src/main/scala/com/services/http/engine/Server.scala
@@ -1,24 +1,30 @@
 package com.services.http.engine
 
-import akka.actor.ActorSystem
+import akka.actor
+import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.Http
 import com.services.http.dtos.JsonFormats
+import com.services.instances.supervisor
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
-object Server extends Routes with JsonFormats {
+object Server  {
+  def main(args: Array[String]): Unit =
+    new Server(ActorSystem[Nothing](supervisor, name = "users-system")).run()
+}
 
-  def main(args: Array[String]) {
-    implicit val system: ActorSystem = ActorSystem("my-system")
-    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+final class Server private[engine] (val system: ActorSystem[Nothing]) extends Routes with JsonFormats{
+  private implicit val classic: actor.ActorSystem = system.classicSystem
+  private implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
+  def run(): Unit = {
     val bindingFuture = Http().bindAndHandle(routes, interface = "0.0.0.0", port = 8080)
-
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
+
   }
 }
